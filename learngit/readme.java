76a8e329edf143b47d4d898cf3f1ee0743836a44
emo-pjy <127082027+emo-pjy@users.noreply.github.com>
2023-06-11 15:09:42 +0800

    Add files via upload

---
+++ learngit/readme.java
@@ -0,0 +1,9 @@
+Git is a version control system.
+Git is free software.
+Hello the world
+<<<<<<< HEAD
+Creating a new branch is quick & simple.
+=======
+Creating a new branch is quick AND simple.
+>>>>>>> feature1
+Creating a new branch is quick and simple.
